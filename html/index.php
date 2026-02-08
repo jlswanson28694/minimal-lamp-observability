3b50c51eca3ac6f47de0b1dddaa57c4b0648c6d9
joe <joexswanson@gmail.com>
2026-02-07 23:10:54 -0600

    Application Exceptions Off By Default

--- html/index.php
+++ html/index.php
@@ -1,11 +1,11 @@
 <?php
-    if(rand(0,10) <= 1) {
-        throw new \Exception();
-    }
+    // For testing Apache status code visualization
+    // if(rand(0,10) <= 1) {
+    //     throw new \Exception();
+    // }
 
-    // echo phpinfo();
+    // Tests the latency histogram
     usleep(rand(2_000, 400_000));
-    // sleep(2);
 ?>
 
 Hello World
